e7e795dc0dfa33c44980f556acdc3fb25f8e4495
le2tang <le2tang@edu.uwaterloo.ca>
2021-01-16 17:23:45 -0500

    Make accessors and mutators extern inline

--- include/map.h
+++ include/map.h
@@ -16,9 +16,10 @@ typedef struct {
 
 extern map_t interest_map;
 
-bool map_inbounds(map_t *map, size_t row, size_t col);
-float map_get(map_t *map, size_t row, size_t col);
-float mat_set(map_t *map, size_t row, size_t col, float data);
+extern inline bool map_inbounds(map_t *map, size_t row, size_t col);
+extern inline float map_get(map_t *map, size_t row, size_t col);
+extern inline float mat_set(map_t *map, size_t row, size_t col, float data);
+extern inline size_t map_get_index(map_t *map, size_t row, size_t col);
 
 void interest_map_init(map_t *map, size_t nrows, size_t ncols, float xdelta, float ydelta);
 void interest_map_update(map_t *map, pose_t robot_pose, float max_interest, float decay);
